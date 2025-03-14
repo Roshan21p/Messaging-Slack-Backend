6f12d900c8ee04cfee624c370f2cdacfe1781996
shendi321 <roshanprajapati2004@gmail.com>
2025-03-14 06:18:25 +0530

    Added db config for mongo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
 
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
@@ -12,4 +16,7 @@ app.get('/ping', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
+
+
